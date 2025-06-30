61528730415d47c700ba1a7fb1d37a32d067e2dd
657002 <imnew108@gmail.com>
2025-06-30 23:29:05 +0700

    add 01

--- 01/main.go
+++ 01/main.go
@@ -1,1 +1,23 @@
-package _1
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+// 01 : Reverse String
+var hello string = "hello"
+
+func main() {
+	splitHello := strings.Split(hello, "")
+	//fmt.Println(splitHello)
+
+	reverseHello := make([]string, len(splitHello))
+	for i, j := 0, len(splitHello)-1; i < len(splitHello); i, j = i+1, j-1 {
+		reverseHello[i] = splitHello[j]
+
+	}
+
+	result := strings.Join(reverseHello, "")
+	fmt.Println(result)
+}
