8b08ad98fb811341c347930e0c67d524f63aa36f
657002 <imnew108@gmail.com>
2025-08-12 16:39:43 +0700

    add leetcode 28

---
+++ logic/leedcode/28_find-the-index-of-the-first-occurrence/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"strings"
+)
+
+/*
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Example 1:
+
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+Example 2:
+
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+
+มี สอง string:
+haystack → ข้อความหลัก (เหมือนกองฟาง)
+needle → ข้อความที่เราต้องการค้นหา (เหมือนเข็มในกองฟาง)
+
+ให้เราหา ตำแหน่งเริ่มต้น (index) ของ needle ใน haystack
+ถ้าไม่เจอ → return -1
+*/
+
+func main() {
+	strStr("sadbutsad", "sad")
+	strStr("leetcode", "leeto")
+}
+
+//
+//func strStr(haystack string, needle string) int {
+//	result := strings.Index(haystack, needle)
+//
+//	return result
+//}
+
+//func strStr(haystack string, needle string) int {
+//	result := strings.Index(haystack, needle)
+//
+//	return result
+//}
+
+func strStr(haystack string, needle string) int {
+	haySpl := strings.Split(haystack, "")
+	needSpl := strings.Split(needle, "")
+
+	for i := 0; i <= len(haySpl)-len(needSpl); i++ {
+		j := 0
+		for ; j < len(needSpl); j++ {
+			if haySpl[i+j] != needSpl[j] {
+				break
+			}
+		}
+		if j == len(needSpl) {
+			return i
+		}
+	}
+
+	return -1
+}
